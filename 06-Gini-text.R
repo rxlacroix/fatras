860245d7c8060cdd83f268fabd5a08e28101d4e9
Romain Lacroix <romain.lacroix@free.fr>
2019-01-28 18:18:41 +0100

    Update 06-Gini-text.R

--- 06-Gini-text.R
+++ 06-Gini-text.R
@@ -3,7 +3,12 @@ library(tidyverse)
 library(tidytext)
 library(ineq)
 
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
 x <-readtext("test.docx")
+swfr <- readtext("https://raw.githubusercontent.com/stopwords-iso/stopwords-fr/master/stopwords-fr.txt") %>%
+  unnest_tokens(word, text)
+
 
 tidy <- x %>%
   unnest_tokens(word, text)
@@ -15,3 +20,14 @@ g <- round(ineq(t$Freq, type = "Gini"),3)
 vn <- round(nrow(t)/nrow(tidy),3)
 
 plot(Lc(t$Freq),col="red",lwd=2, main = paste0("Mots différents = ", nrow(t),"   Gini = ", g, "   V/N = ",vn))
+
+tidyclean <- tidy %>%
+  filter(word %!in% swfr$word)
+
+tclean <- as.data.frame(table(tidyclean$word))
+
+gclean <- round(ineq(tclean$Freq, type = "Gini"),3)
+
+vnclean <- round(nrow(tclean)/nrow(tidyclean),3)
+
+plot(Lc(tclean$Freq),col="red",lwd=2, main = paste0("Mots différents = ", nrow(tclean),"   Gini = ", gclean, "   V/N = ",vnclean))
